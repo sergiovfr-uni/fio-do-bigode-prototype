96c36e5a6201c35c349a861fe39025f3cd3976f6
sergiovfr-uni <sergio.viniciusf@gmail.com>
2026-08-19 14:26:23 -0300

    fix: add Laravel logging configuration

---
+++ backend/laravel/config/logging.php
@@ -0,0 +1,15 @@
+<?php
+use Monolog\Handler\NullHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\SyslogUdpHandler;
+use Monolog\Processor\PsrLogMessageProcessor;
+return [
+    'default' => env('LOG_CHANNEL', 'stderr'),
+    'deprecations' => ['channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'), 'trace' => env('LOG_DEPRECATIONS_TRACE', false)],
+    'channels' => [
+        'stderr' => ['driver' => 'monolog', 'level' => env('LOG_LEVEL', 'debug'), 'handler' => StreamHandler::class, 'handler_with' => ['stream' => 'php://stderr'], 'processors' => [PsrLogMessageProcessor::class]],
+        'single' => ['driver' => 'single', 'path' => storage_path('logs/laravel.log'), 'level' => env('LOG_LEVEL', 'debug'), 'replace_placeholders' => true],
+        'null' => ['driver' => 'monolog', 'handler' => NullHandler::class],
+        'emergency' => ['path' => storage_path('logs/laravel.log')],
+    ],
+];
